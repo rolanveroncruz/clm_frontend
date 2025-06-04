d1a16f79abee565b7061273b3ecec73874f7456d
rolanveroncruz <rolanveroncruz@gmail.com>
2025-06-04 23:12:05 +0800

    logging in working

---
+++ src/app/login/login-header/login-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-header',
+  imports: [],
+  templateUrl: './login-header.html',
+  styleUrl: './login-header.scss'
+})
+export class LoginHeader {
+
+}
